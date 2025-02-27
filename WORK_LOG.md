9dc27e570291d1b25ac6edc4c15bca419b245a4b
damianayala <damian.villarreal-ayala@menloschool.org>
2025-02-27 10:06:02 -0800

    1st commit

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,10 @@
-Name: YOUR NAME HERE
+Name: Damian Villarreal-Ayala 
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date    |  Time   |                                                                                                                                                                                                        Update |
+|:--------|:-------:|--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| April 5 | 9-10pm  |                                                                                                                                                                    I tried to write abstract, efficient code. |
+| Feb 25  | 11-12pm |                                                                                      Spent time thinking through the problem and writing notes to help me understand the problem and ideate ways to solve it. |
+| Feb 27  | 9-10am  | Spent most of class trying to think thru and understand the idea behind checking the diagonal/(left n up). Watched and worked at the board for the majority of the class and started to fill in the 2d array. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/PlagiarismChecker.java
+++ src/PlagiarismChecker.java
@@ -3,21 +3,45 @@
  * A tool for finding the longest shared substring between two documents.
  *
  * @author Zach Blick
- * @author YOUR NAME HERE
+ * @author Damian Villarreal-Ayala
  */
 public class PlagiarismChecker {
 
     /**
      * This method finds the longest sequence of characters that appear in both texts in the same order,
      * although not necessarily contiguously.
+     * This means that the run of similar letters can skip some letters, and it doesn't have to go letter by letter.
      * @param doc1 the first document
      * @param doc2 the second
      * @return The length of the longest shared substring.
+     *
+     * Idea Bank:
+     * - Creating/filling a 2D array that holds the longestShareSubstring up until the char.
+     *  - If we use the table, we can only increase longestSubstringLength by going down and to the right
+     *  while traversing table, otherwise we'd be moving backwards through one of the strings.
+     *  - Doesn't matter of there's a gap between columns when adding to LongestSubstringLength so long as next match is below.
      */
     public static int longestSharedSubstring(String doc1, String doc2) {
 
         // TODO Complete this function to return the length of the longest shared substring.
+        //Adding one to the length to include a "buffer"/empty space that can serve as "base cases".
+        int[][] inspectionTable = new int[doc1.length() + 1][doc2.length() + 1];
+        int  fill = 0;
+        //Goes through and fills first row and column with 0.
+        while(fill < doc1.length())
+        {
+            inspectionTable[fill][0] = 0;
+            fill++;
+        }
+        fill = 0;
+        while(fill < doc2.length())
+        {
+            inspectionTable[0][fill] = 0;
+            fill++;
+        }
+
 
         return 0;
     }
+
 }
